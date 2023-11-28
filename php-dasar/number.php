f691a69a06830a275950e0e763477e0c9d5a17b5
ahmaddzidnii <zdngaming312@gmail.com>
2023-11-28 17:37:51 +0700

    number

---
+++ php-dasar/number.php
@@ -0,0 +1,12 @@
+<?php
+
+echo "Desimal:"; //Basis 10
+var_dump(1234);
+echo "Oktal:"; //Basis 8
+var_dump(0123);
+echo "Hexadesimal:"; //Basis 16
+var_dump(0x1A);
+echo "Binary:"; //Basis 2
+var_dump(0b11111111);
+echo "Undescore in number:"; // Underscore
+var_dump(1_234_567);
