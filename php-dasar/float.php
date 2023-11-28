a4e8a10846a102ab4c0b1b91eb63318f3b62d58a
ahmaddzidnii <zdngaming312@gmail.com>
2023-11-28 17:46:16 +0700

    float

---
+++ php-dasar/float.php
@@ -0,0 +1,15 @@
+<?php
+
+echo "ini adalah tipe data float : "; // float
+var_dump(10.1);
+
+
+echo "e notasion plus dalam float : "; // 10 pangkat 4
+var_dump(2.1e4);
+
+echo "e notasion minus dalam float : "; // 10 pangkat -4
+var_dump(2.1e-4);
+
+
+echo "Undescore floating point : "; // 1.000_000_000
+var_dump(1_000_000_000.123);
