cc8e7cef3f1826805602f8c3e22c875b085416ec
ahmaddzidnii <zdngaming312@gmail.com>
2023-11-28 17:53:08 +0700

    boolean

---
+++ php-dasar/boolean.php
@@ -0,0 +1,11 @@
+<?php
+
+
+//Nilai benar direpresentasikan dengan true (case insensitive)
+//Nilai salah direpresentasikan dengan false (case insensitive)
+
+echo "Benar:";
+var_dump(true); // true nya case insensitive
+
+echo "Salah:";
+var_dump(false); // false nya case insensitive
