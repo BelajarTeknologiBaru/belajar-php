f7ecac2e7107096af970e06c90438bb2a0dff3df
ahmaddzidnii <zdngaming312@gmail.com>
2023-12-02 13:46:37 +0700

    operator increment dan decrement

---
+++ php-dasar/increment-decrement.php
@@ -0,0 +1,14 @@
+<?php
+
+// post increment
+$a = 10;
+$b = $a++;
+var_dump($a); // output 11
+var_dump($b); // output 10
+
+
+// pre increment
+$c = 10;
+$d = ++$c;
+var_dump($c); // output 11
+var_dump($d); // output 11
