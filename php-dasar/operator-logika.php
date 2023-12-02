796a31c1792efab65c41029ad8cdb7bacf80940b
ahmaddzidnii <zdngaming312@gmail.com>
2023-12-02 13:38:05 +0700

    operator logika

---
+++ php-dasar/operator-logika.php
@@ -0,0 +1,10 @@
+<?php
+
+var_dump(true && true); // output true
+var_dump(true && false); // output false
+
+var_dump(true || true); // output true
+var_dump(true || false); // output true
+
+var_dump(true xor true); // output false
+var_dump(true xor false); // output true
